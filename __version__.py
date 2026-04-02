20eea74f3c85100275534cc2c011a50e268cc6de
kim <35963228+king54346@users.noreply.github.com>
2026-04-02 18:46:52 +0800

    Delete __version__.py

--- __version__.py
+++
@@ -1,8 +0,0 @@
-"""To enable `lerobot.__version__`"""
-
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("lerobot")
-except PackageNotFoundError:
-    __version__ = "unknown"
